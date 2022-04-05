e2971155e248a7f2bcddc399b86fc2a50add0f6e
TarleyRodrigues <tarley.divino@gmail.com>
2022-04-05 20:53:51 -0300

    Criando Stream de String (nomes)

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,9 +10,13 @@ public class Program {
 
 		List<Integer> list = Arrays.asList(3, 4 , 5, 10, 14);
 		
+		//Criando uma Stream e usando map para multiplicar cada elemento da stream por 10
 		Stream<Integer> st1 = list.stream().map(x -> x * 10);
-		
 		System.out.println(Arrays.toString(st1.toArray()));
+		
+		//criando uma stream de Strings com nomes;
+		Stream<String> st2 = Stream.of("Maria","Alex","Bob");
+		System.out.println(Arrays.toString(st2.toArray()));
 
 	}
 
